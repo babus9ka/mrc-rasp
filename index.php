2ad4bd51828156ec8fe63fd32473f6382b93b9b5
babus9ka <babus9ka2017@yandex.ru>
2022-09-14 21:55:16 +0300

    added content part

--- index.php
+++ index.php
@@ -2,14 +2,16 @@
 require 'simple_html_dom.php';
 class Scrap{
 
-    function scraping_generic($url, $search) {
+    function scraping_generic($url) {
         $return = array();
         $html = file_get_html($url);
         foreach($html->find('a#rasp') as $e) 
-            $return[] = ["schedule" => $e->href];
+            $return[] = ["schedule" => $e->href,
+                         "content" => $e->find('text')];
 
         foreach($html->find('a#rasp_old') as $e)
-            $return[] = ["schedule_old" => $e->href];
+            $return[] = ["schedule_old" => $e->href,
+                        "content" => $e->find('text')];
 
         $html->clear();
         unset($html);
@@ -20,8 +22,19 @@ class Scrap{
 }
 
 $new = new Scrap();
-$data =  $new->scraping_generic('https://mrk-bsuir.by/ru', '#rasp[href]');
+$data =  $new->scraping_generic('https://mrk-bsuir.by/ru');
+
 $data = array("data" => $data);
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
-echo json_encode($data);
+
+for ($i = 0; $i < count($data['data']); $i++){
+    $content = $data['data'][$i]['content'][1]; 
+    $content = (array)$content;
+    $text = $content['_'][4];
+    unset($data['data'][$i]['content'][0]);
+    unset($data['data'][$i]['content'][1]);
+    $data['data'][$i]['content'] = $text;
+}
+
+echo json_encode($data, JSON_UNESCAPED_UNICODE);
